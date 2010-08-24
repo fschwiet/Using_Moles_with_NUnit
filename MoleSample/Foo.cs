e8487ac74fefeef923ea03424f23853ee81d6b97
fschwiet <fschwiet@gmail.com>
2010-08-23 22:10:49 -0700

    first commit- a sample DLL project with NUnit test DLL

---
+++ MoleSample/Foo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MoleSample
+{
+    public class Foo
+    {
+        public string ConfigDirectory;
+
+        public Stream GetTextFile(string name)
+        {
+            return File.Open(ConfigDirectory + name + ".txt", FileMode.Open);
+        }
+    }
+}
---
+++ MoleSampleTest/FooTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MoleSampleTest
+{
+    [TestFixture]
+    public class FooTest
+    {
+        [Test]
+        public void passes_correct_filename()
+        {
+            // need to verify the right filename is passed in
+        }
+    }
+}
